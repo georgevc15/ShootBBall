4c63a921e7b896f53933e7dadbce1b2bd126a822
George Codat Tudoran <office@solcreation.ro>
2017-03-13 12:19:54 +0200

    using multiply scenes

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartGame();
+        }
+    }
+
+    void StartGame()
+    {
+            //Load Scene
+            int currentIndex = SceneManager.GetActiveScene().buildIndex;
+            //Load currentIndex + 1
+            SceneManager.LoadScene(currentIndex + 1);
+    }
+
+}
