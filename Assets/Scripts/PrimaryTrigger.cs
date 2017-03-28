335868389066cb99d20e416c313e7c25472812a0
George Codat Tudoran <office@solcreation.ro>
2017-03-28 10:30:06 +0300

    Using triggers to detect scoring

---
+++ Assets/Scripts/PrimaryTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrimaryTrigger : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        SecondaryTrigger trigger = GetComponentInChildren<SecondaryTrigger>();
+        trigger.ExpectCollider(collider);
+    }
+
+}
